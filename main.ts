dcc44c3b5134e25490fc0f2a6a09f0b25dde6c87
PCARJ <98421558+PCARJ@users.noreply.github.com>
2022-01-27 13:58:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onGesture(Gesture.Shake, function () {
+    seconds = 0
+})
+let seconds = 0
+seconds = 0
 basic.forever(function () {
-	
+    if (seconds < 10) {
+        seconds += 1
+        basic.pause(1000)
+    } else {
+        robotbit.GeekServo(robotbit.Servos.S1, 181)
+        seconds = 0
+    }
 })
